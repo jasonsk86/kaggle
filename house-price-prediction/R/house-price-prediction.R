77eb9b74fe56b78e4e841a3583d0a5b687e7b200
Jason <59513650+jasonsk86@users.noreply.github.com>
2020-01-07 16:32:25 +0000

    added feature engineering steps

--- house-price-prediction/R/house-price-prediction.R
+++ house-price-prediction/R/house-price-prediction.R
@@ -6,11 +6,12 @@ library(tidyverse)
 library(caret)
 library(randomForest)
 library(scales)
-
+library(patchwork)
+library(car)
 
 # Global setup ------------------------------------------------------------
 
-theme_set(ggthemes::theme_fivethirtyeight())
+theme_set(ggthemes::theme_economist())
 
 # Load data ---------------------------------------------------------------
 
@@ -23,7 +24,11 @@ test <- test_orig
 
 # Preprocessing -----------------------------------------------------------
 
-# Create a summary table of the features 
+# Here the main steps are:
+  # 1. Create summary of data (unique values, NAs, class etc)
+  # 2. Deal with missing values 
+
+# > Data summary ----  
 train_summary <- train %>% 
   map_dfr(~data.frame(
               unique_values =n_distinct(.),
@@ -32,7 +37,10 @@ train_summary <- train %>%
               ),
           .id = "variable")
 
-# Lots of missing values for pool quality, miscfeature, alley and fence
+
+# > Deal with NAs ----
+
+# Lots of missing values for pool quality, miscfeature, alley and fence etc.
 train_summary %>% 
   arrange(-NAs) %>% 
   head(10)
@@ -45,9 +53,15 @@ train <- train %>%
   mutate_if(is.character, ~replace(., is.na(.), "none")) %>% 
   mutate_if(is.numeric, ~replace(., is.na(.), 0))
 
+
 # EDA ---------------------------------------------------------------------
 
-# > Correlation
+# Here the main things to look at are: 
+  # 1. Correlation
+  # 2. Outliers
+
+
+# > Correlation ----
 correlations <- train %>% 
   select_if(is.numeric) %>% 
   cor() 
@@ -88,15 +102,34 @@ ggplot(pred_resp_cor, aes(x = reorder(feature, cor), y = cor, fill = cor)) +
   # Focus on tree-based methods that are less susceptible to problems around correlation
   # Use some form of penalized regression model 
 
+# > Outliers ----
 
-# Drop variables with next to no correlation with Sale Price
-features_to_drop <- pred_resp_cor %>% 
-  filter(abs(cor) < 0.2) %>% 
-  pull(feature)
+p_out <- ggplot(train, aes(y = SalePrice)) +
+  geom_boxplot() +
+  scale_y_continuous(labels = comma)
 
+p_hist <- ggplot(train, aes(x = SalePrice)) +
+  geom_histogram(colour = "white") +
+  scale_x_continuous(labels = comma)
+
+wrap_elements(p_hist + p_out) + ggtitle("Distribution of House Sale Prices")
+
+# It doesn't look like there are too many exteme values, might come back to this if we notice any problems in modelling
+
+
+# Feature engineering -----------------------------------------------------
+
+
+# > New features ----
+
+# add feature for if there were any remodelling or additions made to the house 
 train <- train %>% 
-  select(-one_of(features_to_drop))
+  mutate(remodeled = ifelse(YearRemodAdd == YearBuilt, "N", "Y"))
 
+
+# > Feature extraction ----
+
+# use PCA to reduce dimensionality and created variables based on principal componts
 
 # Principal Componets Analysis (PCA)
 train_pca <- train %>% 
@@ -127,14 +160,114 @@ pca_var$coord %>%
   coord_flip() +
   facet_wrap(~component)
 
-
 # Scree plot - ~50% of variance explained by first three principal components
 factoextra::fviz_screeplot(train_pca)
 
 # PCA variable plot
 factoextra::fviz_pca_var(train_pca)
 
+# add pc1 and pc2 into dataset 
 train <- train %>% 
   mutate(pc1 = pca_ind$coord[, 1],
-         pc2 = pca_ind$coord[, 2])
+         pc2 = pca_ind$coord[, 2]) %>% 
+  select(-SalePrice, everything(), SalePrice) # so response variable is back at most right column
+
+
+# > Remove features ----
+
+# Drop variables with next to no correlation with Sale Price
+features_to_drop <- pred_resp_cor %>% 
+  filter(abs(cor) < 0.2) %>% 
+  pull(feature)
+
+train <- train %>% 
+  select(-one_of(features_to_drop))
+
+
+# > Binning ----
+
+# Check for any categorical variables that have very low frequencies 
+# Should hopefully make models bit more robust and not overfit to niche examples
+
+var_frequencies <- train %>% 
+  select_if(is.character) %>% 
+  pivot_longer(
+    cols = everything()
+  ) %>% 
+  count(name, value) %>% 
+  
+
+# which variables have sub-attributes with low occurences
+var_frequencies %>%
+  filter(n < 5) %>% 
+  group_by(name) %>% 
+  summarize(low_atts = n_distinct(value)) %>% 
+  arrange(-low_atts)
+
+# conditon2
+  # this relates to the proximity to various conditions (if more than one is present)
+  # here just bin into 'normal' and 'other' to reduce cardinality of variable
+
+train$Condition2 %>% table()
+
+train <- train %>% 
+  mutate(Condition2 = ifelse(Condition2 == "Norm", "Norm", "Other"))
+
+# exterior1st
+  # this relates to the exterior covering the house
+  # here again just group anything under 5 as other 
+train$Exterior1st %>% table()
+
+exterior1st_other <- train %>% 
+  count(Exterior1st) %>% 
+  filter(n < 5) %>% 
+  pull(Exterior1st)
+
+train <- train %>% 
+  mutate(Exterior1st = ifelse(Exterior1st %in% exterior1st_other, "Other", Exterior1st))
+
+# RoofMatl
+train$RoofMatl %>% table()
+
+RoofMatl_other <- train %>% 
+  count(RoofMatl) %>% 
+  filter(n < 5) %>% 
+  pull(RoofMatl)
+
+train <- train %>% 
+  mutate(RoofMatl = ifelse(RoofMatl %in% RoofMatl_other, "Other", RoofMatl))
+
+
+# > Log Transform ---- 
+
+# Create new response value that is log transform of SalePrice. This should help reduce the skewed nature of the data
+# should also decrease the effect of outliers when modelling, particularly with regression techniques
+
+train <- train %>% 
+  mutate(log_SalePrice = log10(SalePrice))
+
+p_log_hist <- ggplot(train, aes(x = log_SalePrice)) +
+  geom_histogram(colour = "white") +
+  labs(x = "log Sale Price")
+
+wrap_elements(p_hist + p_log_hist) + 
+  ggtitle("Distribution of Sale Price Befoe and After Log Transform") 
+
+
+
+# Modelling ---------------------------------------------------------------
+
+
+# > Multiple linear regression -------------------------------------------------------
+
+
+
+
+# Random forest -----------------------------------------------------------
+
+
+
+# Gradient boosting -------------------------------------------------------
+
+
 
